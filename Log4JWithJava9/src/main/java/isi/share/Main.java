89347ba0037a96cb9966d168e7e295ac7ec316ba
matameko <info@matameko.de>
2017-11-10 21:03:42 +0100

    example maven project that uses pom packaging and causes issues in Eclipse Oxygen

---
+++ Log4JWithJava9/src/main/java/isi/share/Main.java
@@ -0,0 +1,14 @@
+package isi.share;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class Main {
+
+    private static Logger sysLog = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Log4JWithJava9/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module Log4JWithJava9 {
+	exports isi.share;	
+	
+	requires javafx.base;
+	requires log4j.api;
+	
+}
